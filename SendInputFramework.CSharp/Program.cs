d2e0353a8fe6cf7426f877dacf1a1205ab56ee6f
Torben Clasen <t.clasen@outlook.com>
2020-01-08 22:49:39 +0100

    added dotnet framework csharp

---
+++ SendInputFramework.CSharp/Program.cs
@@ -0,0 +1,19 @@
+﻿using SendInputLib.CSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SendInputFramework.CSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SendInputBindings.PressKey(SendInputBindings.KeyCodes.KEY_A);
+
+            Console.ReadLine();
+        }
+    }
+}
