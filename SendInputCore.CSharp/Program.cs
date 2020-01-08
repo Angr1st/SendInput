ca8735cb314cb18f6792744f7dfd68ed21cb702a
Torben Clasen <t.clasen@outlook.com>
2020-01-08 22:47:24 +0100

    Add Dotnet core csharp

---
+++ SendInputCore.CSharp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.InteropServices;
+using SendInputLib.CSharp;
+
+namespace SendInputCore.CSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SendInputBindings.PressKey(SendInputBindings.KeyCodes.KEY_A);
+            var error = new Win32Exception(Marshal.GetLastWin32Error());
+
+            Console.WriteLine($"{error.Message}; ErrorCode: {error.NativeErrorCode}");
+        }
+    }
+}
